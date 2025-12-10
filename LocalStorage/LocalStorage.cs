550ef0365b8fc6047159d7e16ccb6a857df63026
lovespite <i@lovespite.com>
2025-12-10 13:04:28 +0800

    添加项目文件。

---
+++ LocalStorage/LocalStorage.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization.Metadata;
+
+namespace LocalStorage;
+
+public abstract class LocalStorage
+{
+    public abstract Task SetAsync(string key, string data);
+    public abstract Task<string?> GetAsync(string key);
+    public abstract Task RemoveAsync(string key);
+    public abstract Task ClearAsync();
+
+    public virtual string? Get(string key)
+    {
+        return GetAsync(key).GetAwaiter().GetResult();
+    }
+
+    public virtual void Set(string key, string data)
+    {
+        SetAsync(key, data).GetAwaiter().GetResult();
+    }
+
+    public virtual void Clear()
+    {
+        ClearAsync().GetAwaiter().GetResult();
+    }
+
+    public virtual void Remove(string key)
+    {
+        RemoveAsync(key).GetAwaiter().GetResult();
+    }
+}
